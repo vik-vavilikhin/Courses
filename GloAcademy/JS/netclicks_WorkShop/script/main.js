f65b1ac7c0ce1e150425cbca256aa7f908119ab2
Viktor Vavilikhin <vva76@yandex.ru>
2020-05-27 12:28:42 +0300

    Update

--- GloAcademy/JS/netclicks_WorkShop/script/main.js
+++ GloAcademy/JS/netclicks_WorkShop/script/main.js
@@ -1,13 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use script';
 
+  const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+  const API_KEY = '3c0c35a5baa5178f9566bfcb61de01a4';
+
   /*===========================================
-                   Элементы DOM
+                 Элементы DOM
   ===========================================*/
   const leftMenu = document.querySelector('.left-menu');
   const hamburger = document.querySelector('.hamburger');
-  const tvCard = document.querySelectorAll('.tv-card');
+  const tvShowsList = document.querySelector('.tv-shows__list');
+  const modal = document.querySelector('.modal');
 
+  /*===========================================
+                Работа с данными
+  ===========================================*/
+  const DBService = class {
+    getData = async (url) => {
+      const res = await fetch(url);
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(`
+        не удалось получить данные по адресу:
+        ${url}
+        `)
+      }
+    }
+    getTestData = () => {
+      return this.getData('test.json');
+    }
+  }
+
+  const renderCard = response => {
+    tvShowsList.textContent = '';
+
+    response.results.forEach(item => {
+      const {
+        "backdrop_path": backdrop,
+        "name": title,
+        "poster_path": poster,
+        "vote_average": vote
+      } = item;
+
+      const posterIMG = poster ?
+        (IMG_URL + poster) :
+        'img/no-poster.jpg';
+
+      const backdropIMG = backdrop ?
+        (IMG_URL + backdrop) :
+        posterIMG;
+
+      const voteElem = (vote != 0) ?
+        `<span class="tv-card__vote">${vote}</span>` :
+        '';
+
+      const card = document.createElement('li');
+      card.className = 'tv-shows__item';
+      card.innerHTML = `
+        <a href="#" class="tv-card">
+          ${voteElem}
+          <img class="tv-card__img"
+            src="${posterIMG}"
+            data-backdrop="${backdropIMG}"
+            alt="${title}">
+          <h4 class="tv-card__head">${title}</h4>
+        </a>
+      `;
+
+      tvShowsList.append(card);
+
+    });
+
+  };
+
+  new DBService().getTestData().then(renderCard);
+
+  /*===========================================
+                  Обработчики
+  ===========================================*/
   // Открыть-закрыть MENU по кнопке
   hamburger.addEventListener('click', () => {
     leftMenu.classList.toggle('openMenu');
@@ -38,34 +109,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Смена картинки при наведении на карточке
-  // через "навешивание" слушателя на каждый элемент
-  // с className = '.tv-card'
-  tvCard.forEach((item) => {
-    let oldSrc;
+  // Смена картинки при наведении на карточку
+  const changeImage = event => {
+    const card = event.target.closest('.tv-shows__item');
 
-    item.addEventListener('mouseover', (event) => {
-      const target = event.target;
-      const image = target.closest('.tv-card__img');
+    if (card) {
+      const img = card.querySelector('.tv-card__img');
 
-      if (image) {
-        // Получить значение текущей картинки
-        // и запомнить в переменной
-        oldSrc = image.src;
-        // Поменять картинку на значение атрибута
-        image.src = image.getAttribute('data-backdrop');
+      if (img.dataset.backdrop) {
+        [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
       }
+    }
+  };
 
-    });
+  tvShowsList.addEventListener('mouseover', changeImage);
+  tvShowsList.addEventListener('mouseout', changeImage);
 
-    item.addEventListener('mouseout', (event) => {
-      const target = event.target;
-      const image = target.closest('.tv-card__img');
+  /*====================================
+               Модальное окно
+  ====================================*/
+  // Открытие
+  tvShowsList.addEventListener('click', (event) => {
+    event.preventDefault();
 
-      if (image) {
-        // вернуть старое значение из переменной
-        image.src = oldSrc;
-      }
-    });
+    const target = event.target;
+    const tvCard = target.closest('.tv-card');
+
+    if (tvCard) {
+      document.body.style.overflow = 'hidden';
+      modal.classList.remove('hide');
+    }
+  });
+  // Закрытие
+  modal.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('modal') ||
+      target.closest('.cross')) {
+      modal.classList.add('hide');
+    }
   });
 });
