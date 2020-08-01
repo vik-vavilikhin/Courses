59bf6ff8278fbe663d3194aa2020e148312da615
Viktor Vavilikhin <vva76@yandex.ru>
2020-08-01 09:52:55 +0300

    Added Logo-shop from FL

---
+++ Freelancer/LOGOiShop/#src/js/script.js
@@ -0,0 +1,19 @@
+// import Swiper from './../vendor/swiper/js/swiper';
+import * as app from './modules/app';
+// import './../layout/s-header/burger';
+// import './../layout/s-features/b-menu-features/menu-features';
+// import './../layout/s-offers/offers';
+import {
+  sliderInit
+} from './modules/slider';
+
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+  // =========================================
+  app.inspectUserAgent();
+  app.testWebP();
+  app.backgroundImage();
+  app.elemReplace();
+  // =========================================
+  sliderInit();
+});
